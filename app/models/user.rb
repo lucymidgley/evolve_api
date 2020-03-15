f82081cab3d367e19978c887c76e9ba7b425bcfb
NoahDumba <noahdumba1@gmail.com>
2020-03-15 15:33:51 -0600

    testing validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, uniqueness: { case_sensitive: false }
-  validates :username, uniqueness: true
-  validates :name, presence: true
-  validates :password, length: { minimum: 5 }, confirmation: true
-  validates :password_confirmation, presence: true
+  # validates :email, uniqueness: { case_sensitive: false }
+  # validates :username, uniqueness: true
+  # validates :name, presence: true
+  # validates :password, length: { minimum: 5 }, confirmation: true
+  # validates :password_confirmation, presence: true
 
 end
