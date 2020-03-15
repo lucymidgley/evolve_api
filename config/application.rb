c81ed7f6c8416258bfbc62889e898c7f90550c13
Noah Dumba <noahdumba1@gmail.com>
2020-03-15 21:07:51 +0000

    trying to fix cors still

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module EvolveApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: :any
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
   end
