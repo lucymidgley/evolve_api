24a6024fd1e36fd14afed27bef434056d1b82c41
Noah Dumba <noahdumba1@gmail.com>
2020-03-15 00:55:37 +0000

    Revert "add validations for registering user" reverting to fix server
    This reverts commit a722fa84411645dc5313e776685900a4281285f4.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,2 @@
 class User < ApplicationRecord
-  has_secure_password
-
-  validates :email, uniqueness: { case_sensitive: false }
-  validates :username, uniqueness:
-  validates :password, length: { minimum: 5 }, confirmation: true
-  validates :password_confirmation, presence: true
-  before_save :format_email
-
-  def format_email
-    self.email.downcase!
-    self.email.strip!
-  end
-
-  def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email.downcase.strip)
-
-    if user && user.authenticate(password)
-      return user
-    else
-      return nil
-    end
-  end
 end
