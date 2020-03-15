386284f2a19bc1a31e3307f4dd21ba1d60e92b7a
Noah Dumba <noahdumba1@gmail.com>
2020-03-15 01:12:56 +0000

    i dont know

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: { case_sensitive: false }
-  validates :username, uniqueness:
+  validates :username, uniqueness: true
   # validates :name, presence: true
   # validates :password, length: { minimum: 5 }, confirmation: true
   # validates :password_confirmation, presence: true
