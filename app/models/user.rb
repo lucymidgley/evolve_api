23ae20ed8673c25ea67dcbc75d97e832b92412a3
Noah Dumba <noahdumba1@gmail.com>
2020-03-15 01:06:06 +0000

    add other validations to see if heroku breaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: { case_sensitive: false }
- 
+  validates :username, uniqueness:
+  validates :password, length: { minimum: 5 }, confirmation: true
+  validates :password_confirmation, presence: true
+
 end
